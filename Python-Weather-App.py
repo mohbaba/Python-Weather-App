184c32135cd24a8dcb3e20d82ac46041865745e6
mohbaba <babamu09@gmail.com>
2023-01-07 23:22:29 +0100

    Location

--- Python-Weather-App.py
+++ Python-Weather-App.py
@@ -4,6 +4,15 @@ from flet import *
 import requests
 import datetime
 import geopy
+from geopy.geocoders import Nominatim
 
+
+# Nominatim is an api that gets the data needed after getting the user's address
+loc = Nominatim(user_agent = "Python Weather App")
+location = str(input("Please type in your location, State and Country "))
+getLoc = loc.geocode(location)
+lat= getLoc.latitude
+lon = getLoc.longitude
+print(f'{lat},{lon}')
 
 api_key = "b525ab6dd052055c4fc36e2ea7794cc4"
